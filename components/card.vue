69432c61e8617f5045bb434527d73f4d4c3fe564
Blacklivi <blacklivi@yandex.by>
2021-03-12 19:45:53 +0300

    task_One

---
+++ components/card.vue
@@ -0,0 +1,75 @@
+<template>
+  <div>
+    <div
+      v-for="(item, key) in subjects"
+      :key="key"
+    >
+      <div
+        v-if="item.type === type"
+      >
+        {{ item.name }}
+      </div>
+    </div>
+
+    <v-text-field
+      placeholder="name"
+      v-model="search"
+    >
+
+    </v-text-field>
+
+    <ul>
+      <li v-for="(i, key) in filterTwo" :key="key">
+        {{ i.name }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['subjects'],
+  data() {
+    return {
+      type: '',
+      search: ''
+    }
+  },
+  computed: {
+    filter() {
+      if (!this.type) {
+        return []
+      } else {
+        return this.subjects.filter(obj => {
+          return obj.type === this.type
+        })
+      }
+    },
+    filterTwo() {
+      if (this.filter.length === 0) {
+        return []
+      } else if (this.search === '') {
+        return this.filter
+      } else {
+        return this.filter.filter(obj => {
+            return obj.name.toLowerCase() === this.search.toLowerCase()
+          }
+        )
+      }
+    }
+  },
+  created() {
+    console.log(this.subjects);
+    console.log(this.$route);
+    this.type = this.$route.query.type
+  }
+}
+</script>
+
+<style scoped>
+.myText {
+  border: 1px solid black;
+  border-radius: 4px;
+  margin-top: 1rem;
+}
+</style>
---
+++ pages/category.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <Card
+    :subjects="subjects"
+    />
+  </div>
+</template>
+
+<script>
+import Card from '@/components/card'
+
+export default {
+  components: {
+    Card
+  },
+  data() {
+    return {
+      subjects: [
+        {
+          type: 'car',
+          name: 'BMW'
+        },
+        {
+          type: 'car',
+          name: 'Audi'
+        },
+        {
+          type: 'car',
+          name: 'Citroen'
+        },
+        {
+          type: 'fruit',
+          name: 'apple'
+        },
+        {
+          type: 'fruit',
+          name: 'melissa'
+        },
+        {
+          type: 'fruit',
+          name: 'banana'
+        },
+        {
+          type: 'animals',
+          name: 'cat'
+        },
+        {
+          type: 'animals',
+          name: 'dog'
+        },
+        {
+          type: 'animals',
+          name: 'elephant'
+        }
+      ]
+    }
+  },
+}
+</script>
+
+<!--<style scoped>-->
+
+<!--</style>-->
